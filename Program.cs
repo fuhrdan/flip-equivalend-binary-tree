c46ee9cdf93d0f8da9b0071a146b2a609a5da33c
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-10-24 14:27:59 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//*****************************************************************************
+//** 951. Flip Equivalent Binary Trees    leetcode                           **
+//*****************************************************************************
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool dfs(struct TreeNode* root1, struct TreeNode* root2) {
+    if (root1 == root2 || (root1 == NULL && root2 == NULL)) {
+        return true;
+    }
+    if (root1 == NULL || root2 == NULL || root1->val != root2->val) {
+        return false;
+    }
+    return (dfs(root1->left, root2->left) && dfs(root1->right, root2->right)) ||
+           (dfs(root1->left, root2->right) && dfs(root1->right, root2->left));
+}
+
+bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2) {
+    return dfs(root1, root2);
+}
